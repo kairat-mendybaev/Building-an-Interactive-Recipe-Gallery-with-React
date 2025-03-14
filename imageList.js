4f16588c86d20f92ca902e30021ac1dcb7d942ab
Kairat Mendybaev <113317646+kairat-mendybaev@users.noreply.github.com>
2025-03-14 00:06:15 -0700

    imageList.js

---
+++ imageList.js
@@ -0,0 +1,14 @@
+// src/imageList.js
+export const images = [
+  {
+    id: 1,
+    url: 'https://example.com/image1.jpg',
+    description: 'Image 1 Description'
+  },
+  {
+    id: 2,
+    url: 'https://example.com/image2.jpg',
+    description: 'Image 2 Description'
+  },
+  // Add more images as needed
+];
